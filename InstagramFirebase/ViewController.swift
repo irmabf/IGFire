805bea42e4efdd4556888186fbfdb5babf06469a
Irma <irblancofdz@gmail.com>
2018-06-29 20:54:00 +0200

    Initial Commit

---
+++ InstagramFirebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramFirebase
+//
+//  Created by Irma Blanco on 29/06/2018.
+//  Copyright © 2018 Irma Blanco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
