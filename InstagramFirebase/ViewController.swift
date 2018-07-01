f68c650cc8a96013be1ccf77f65e47f3d682b979
Irma <irblancofdz@gmail.com>
2018-07-01 15:17:25 +0200

    Finish SignUp screen and Color and Anchor extensions

--- InstagramFirebase/ViewController.swift
+++ InstagramFirebase/ViewController.swift
@@ -9,17 +9,95 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  let plusPhotoButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+    return button
+  }()
+  
+  let emailTextField: UITextField = {
+    let tf = UITextField()
+    tf.translatesAutoresizingMaskIntoConstraints = false
+    tf.placeholder = "Email"
+    tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+    tf.borderStyle = .roundedRect
+    tf.font = UIFont.systemFont(ofSize: 14)
+    return tf
+  }()
+  
+  let usernameTextField: UITextField = {
+    let tf = UITextField()
+    tf.placeholder = "Username"
+    tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+    tf.borderStyle = .roundedRect
+    tf.font = UIFont.systemFont(ofSize: 14)
+    return tf
+  }()
+  
+  let passwordTextField: UITextField = {
+    let tf = UITextField()
+    tf.isSecureTextEntry = true
+    tf.placeholder = "Password"
+    tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+    tf.borderStyle = .roundedRect
+    tf.font = UIFont.systemFont(ofSize: 14)
+    return tf
+  }()
+  
+  let signUpButton: UIButton = {
+    let button = UIButton(type: .system)
+
+    button.setTitle("Sign Up", for: .normal)
+    button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+    button.layer.cornerRadius = 5
+    button.setTitleColor(.white, for: .normal)
+    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+    return button
+  }()
+  
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    view.addSubview(plusPhotoButton)
+    
+    setupInputFields()
+    
+    plusPhotoButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 40, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 140, height: 140)
+    
+    plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+
+  fileprivate func setupInputFields() {
+    let greenView = UIView()
+    greenView.backgroundColor = .green
+    
+    let redView = UIView()
+    redView.backgroundColor = .red
+    
+    let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField, signUpButton])
+    
+  
+    stackView.distribution = .fillEqually
+    stackView.axis = .vertical
+    stackView.spacing = 10
+    view.addSubview(stackView)
+    
+    stackView.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 20, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 200)
   }
-
-
+  
 }
+
+
+
+
+
+
+
+
+
+
 
