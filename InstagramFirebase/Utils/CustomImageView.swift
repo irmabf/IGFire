d9d6628b26629853fdd53622d82a2e925ae784e9
Irma <irblancofdz@gmail.com>
2018-07-05 18:25:24 +0200

    Add CustomImageView and perform some refactor

---
+++ InstagramFirebase/Utils/CustomImageView.swift
@@ -0,0 +1,40 @@
+//
+//  CustomImageView.swift
+//  InstagramFirebase
+//
+//  Created by Irma Blanco on 05/07/2018.
+//  Copyright © 2018 Irma Blanco. All rights reserved.
+//
+
+import UIKit
+
+class CustomImageView: UIImageView {
+  
+  var lastURLUsedToLoadImage: String?
+  
+  func loadImage(urlString: String){
+    print("Loading Image")
+    
+    lastURLUsedToLoadImage = urlString
+    
+    guard let url = URL(string: urlString) else { return }
+    
+    URLSession.shared.dataTask(with: url) { (data, response, err) in
+      if let err = err {
+        print("Failed to fetch post image:", err)
+        return
+      }
+      
+      if url.absoluteString != self.lastURLUsedToLoadImage {
+        return
+      }
+      guard let imageData = data else { return }
+      
+      let photoImage = UIImage(data: imageData)
+      
+      DispatchQueue.main.async {
+        self.image = photoImage
+      }
+      }.resume()
+  }
+}
